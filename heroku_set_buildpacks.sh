bba89645a2a76b4c8ea2570344113fcc57956ea6
Rodrigo Rodrigues da Silva <pitanga@members.fsf.org>
2016-02-05 20:18:20 -0200

    gettext buildpack and post_compile hooks

--- heroku_set_buildpacks.sh
+++ heroku_set_buildpacks.sh
@@ -1,2 +1,4 @@
+heroku buildpacks:clear
 heroku buildpacks:set https://github.com/heroku/heroku-buildpack-python
 #heroku buildpacks:add https://github.com/andreipetre/heroku-buildpack-django-migrate
+heroku buildpacks:add https://github.com/CBien/heroku-buildpack-gettext --index 1
