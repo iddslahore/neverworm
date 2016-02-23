07b7f3ce64a5f41a96077edd6a5c9d6c7a8db464
Rodrigo Rodrigues da Silva <pitanga@members.fsf.org>
2016-02-23 16:23:32 -0300

    Switch default language back to English

--- neverworm/settings.py
+++ neverworm/settings.py
@@ -164,7 +164,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'ur'
+#LANGUAGE_CODE = 'ur'
 
 LANGUAGES = (
     ('en', 'English'),
